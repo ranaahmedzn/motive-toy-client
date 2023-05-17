c22b9f782cb766f771702786b0c20a435cd6af01
Rana-Sheikh <ranasheikhsbd@gmail.com>
2023-05-18 00:01:46 +0600

    initial setup with firebase config

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="text-center my-3">
+      <h1 className="text-4xl font-bold my-4">Welcome to Motive Toy</h1>
+      <button className="btn btn-success">Button</button>
+    </div>
+  )
+}
+
+export default App
